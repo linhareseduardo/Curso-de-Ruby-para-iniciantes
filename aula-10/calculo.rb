75e9067ef038f01480e7eaf85538a936e362d565
linhareseduardo <linhareseduardo7@gmail.com>
2023-04-04 16:56:53 -0300

    aula 10 métodos

---
+++ aula-10/calculo.rb
@@ -0,0 +1,20 @@
+def soma (x, y, z)
+     x + y + z
+end
+
+def subtracao (x, y)
+    x - y
+end
+
+def multiplicacao (x, y)
+    x * y
+end
+
+def divisao(x, y)
+    (x.to_f / y.to_f)
+end
+
+puts soma(4,5,2)
+puts subtracao(58,6)
+puts multiplicacao(2,3)
+puts divisao(1,2)
