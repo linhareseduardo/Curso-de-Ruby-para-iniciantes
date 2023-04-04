ebd47d018a701288fb7e4c3781ce385ed91b5095
linhareseduardo <linhareseduardo7@gmail.com>
2023-04-04 16:11:04 -0300

    aula 08 repetições - for, while, times, loop

---
+++ aula-08/do_while.rb
@@ -0,0 +1,7 @@
+count = 1 
+
+loop do
+    puts "Diego"
+    break if count == 10
+    count +=1
+end
---
+++ aula-08/while.rb
@@ -0,0 +1,10 @@
+nomes = ["João", "Maria", "José", "Mateus"]
+
+
+count = 0
+
+while count < nomes.size do
+    puts nomes[count]
+    puts count
+    count +=1
+end
