89bcddee1d9abda1e738b9f5a87bd916a5956b0a
linhareseduardo <linhareseduardo7@gmail.com>
2023-04-04 08:50:11 -0300

    aula 02 números e operações matemáticas

---
+++ aula-02/numeros.rb
@@ -0,0 +1,14 @@
+puts "Insira um numero"
+x = gets.chomp.to_i
+puts "Insira outro numero" 
+y = gets.chomp.to_i
+
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x.to_f / y.to_f
+
+puts soma
+puts subtracao
+puts multiplicacao
+puts divisao
